2dddf753d4bd531d0bb33d959d3acbe846b0a6d5
amar-Shifan <amarshifaanvp@gmail.com>
2024-08-16 00:44:46 +0530

    popUp

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
 
 
+let div = document.getElementById('div')
+let temp = 1 ;
 
-let count = 0;
-let image2 = "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/f871c3dec2984ff3.jpeg?q=20";
-let image3 = "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/b35a105fe8bc8cbb.png?q=20";
-
-let tempcount = 0; 
-let right_arrow = document.getElementById('arrow_right');
-let left_arrow = document.getElementById('arrow_left');
-
-function arrow(direction){
-
-
-    let div = document.getElementById('container-slider');
-    let image = document.getAnimations('image');
-    console.log(image);
-
-    console.log(div);
-
-    if(direction){
-        console.log("its right");
-        
-        image.src = "/mobile.png";
-
+function popUp(){
+    if(temp==1){
+        div.style.display = 'flex';
+        temp = 0;
     }else{
-        console.log("its left");
-        image.src = image3
+        div.style.display = 'none';
+        temp = 1;
     }
 }
-
